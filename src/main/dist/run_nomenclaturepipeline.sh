82b6f42ac1ca4bbf8c642f4daa2a37757f3aa5af
tutajm <mtutaj@mcw.edu>
2025-12-15 10:33:37 -0600

    updated dependencies

--- src/main/dist/run_nomenclaturepipeline.sh
+++ src/main/dist/run_nomenclaturepipeline.sh
@@ -8,7 +8,7 @@ $APPHOME/_run.sh > $APPHOME/run.log 2>&1
 #send summary email
 EMAIL=mtutaj@mcw.edu
 if [ "$SERVER" = "REED" ]; then
-  EMAIL_LIST=rgd.devops@mcw.edu,jrsmith@mcw.edu,slaulederkind@mcw.edu
+  EMAIL_LIST="rgd.devops@mcw.edu jrsmith@mcw.edu slaulederkind@mcw.edu"
 fi
 mailx -s "[$SERVER] NomenclaturePipeline ok" $EMAIL < ${APPHOME}/logs/summary.log
 
